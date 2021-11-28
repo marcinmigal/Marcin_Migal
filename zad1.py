0ab87b4c0fe3038f204ec4db314e8da04e68650a
Marc <marc.mig@hotmail.com>
2021-11-28 15:55:01 +0100

    Pep8 Correction

--- zad1.py
+++ zad1.py
@@ -4,12 +4,14 @@ class Student:
         self.marks = marks
 
     def is_passed(self) -> bool:
-        if self.marks > 50: return True
-        else: return False
+        if self.marks > 50:
+            return True
+        else:
+            return False
 
 
-student_1 = Student('Adam',64)
-student_2 = Student('Jan',31)
+student_1 = Student('Adam', 64)
+student_2 = Student('Jan', 31)
 
 student_1.is_passed()
 student_2.is_passed()
--- zad3.py
+++ zad3.py
@@ -1,6 +1,6 @@
 class Property:
 
-    def __init__(self, area:str, rooms:int, price:float, address:str):
+    def __init__(self, area: str, rooms: int, price: float, address: str):
         self.area = area
         self.rooms = rooms
         self.price = price
@@ -9,24 +9,41 @@ class Property:
 
 class House(Property):
 
-    def __init__(self, area: str, rooms: int, price: float, address: str, plot:int):
+    def __init__(
+            self,
+            area: str,
+            rooms: int,
+            price: float,
+            address: str,
+            plot: int):
         super().__init__(area, rooms, price, address)
         self.plot = plot
-    def __str__(self) -> str:
-        return f'House located in {self.area} area with {self.rooms} rooms, property size: {self.plot}. Price: {self.price}, Address: {self.address}'
 
+    def __str__(self) -> str:
+        return f'House located in {self.area} area with {self.rooms} rooms,\
+             property size: {self.plot}. Price: {self.price},\
+                  Address: {self.address}'
 
 
 class Flat(Property):
-    def __init__(self, area: str, rooms: int, price: float, address: str, floor:str):
+    def __init__(
+            self,
+            area: str,
+            rooms: int,
+            price: float,
+            address: str,
+            floor: str):
         super().__init__(area, rooms, price, address)
         self.floor = floor
-    
-    def __str__(self) -> str:
-        return f'Flat located in {self.area} area with {self.rooms} rooms, placed at level {self.floor}. Price: {self.price}, Address: {self.address}'
 
-house_1 = House('Central', 7, 920000, 'Main Street 12/45',800)
-flat_1 = Flat('Central', 3, 420000, '9th Street 5/13',4)
+    def __str__(self) -> str:
+        return f'Flat located in {self.area} area with {self.rooms} \
+            rooms, placed at level {self.floor}. Price: {self.price}\
+                , Address: {self.address}'
+
+
+house_1 = House('Central', 7, 920000, 'Main Street 12/45', 800)
+flat_1 = Flat('Central', 3, 420000, '9th Street 5/13', 4)
 
 print(house_1)
 print(flat_1)
