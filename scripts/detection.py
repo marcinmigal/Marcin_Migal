3fbc50797b7bacbbf704fb51648d8af70cb0d509
unknown <marcin.migal@edu.uekat.pl>
2022-01-22 15:13:30 +0100

    Person Detection

--- scripts/detection.py
+++ scripts/detection.py
@@ -3,12 +3,15 @@ import imutils
 import cv2
 
 
-def detect_person(hog, paths):
+def detect_person(paths):
+    hog = cv2.HOGDescriptor()
+
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
     for imagePath in paths:
         image = cv2.imread(imagePath)
         time_start = cv2.getTickCount()
         image = imutils.resize(image, width=min(650, image.shape[1]))
-        # image = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
         (rects, weights) = hog.detectMultiScale(image, winStride=(6, 6),
                                                 padding=(10, 10), scale=1.05)
 
@@ -19,7 +22,7 @@ def detect_person(hog, paths):
         cv2.putText(image, f'Detection Took:'
                     f'{(time_end - time_start)/cv2.getTickFrequency()} ',
                     (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 0, 0), 2)
-        cv2.putText(image, f'Total Persons : {len(rects)}',
+        cv2.putText(image, f'People Count : {len(rects)}',
                     (40, 10), cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 0, 0), 2)
 
         cv2.imshow("Detection Results", image)
